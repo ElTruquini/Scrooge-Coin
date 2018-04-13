97ba8736b689f4205eb9f359f5941c4bd2ead9df
Daniel <daniel.olayam@gmail.com>
2018-04-13 12:07:49 -0700

    testing, added genesis coin

--- Scrooge_main.java
+++ Scrooge_main.java
@@ -1,4 +1,3 @@
-//Written by: Daniel Olaya
 //Date: April, 2018
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
@@ -41,13 +40,32 @@ public class Scrooge_main{
 		System.out.printf("Wuillie public key: %s\n",printKey(pub_Wuille.getEncoded()));
 		System.out.println();
 
-		//Generating Sodler keys
+		//Generating Hodler keys
 		pair = keyGen.generateKeyPair();
 		PrivateKey priv_Sodler = pair.getPrivate();
 		PublicKey pub_Sodler = pair.getPublic();
-		System.out.printf("Sodler private key: %s\n",printKey(priv_Sodler.getEncoded()));
-		System.out.printf("Sodler public key: %s\n",printKey(pub_Sodler.getEncoded()));
-	}	
+		System.out.printf("Hodler private key: %s\n",printKey(priv_Sodler.getEncoded()));
+		System.out.printf("Hodler public key: %s\n",printKey(pub_Sodler.getEncoded()));
+
+		//Creating genesis tx
+		Transaction tx = new Transaction();
+		tx.addOutput(10, pub_Wuille);
+		byte[] initial_hash = BigInteger.valueOf(1695609641).toByteArray();
+		tx.addInput(initial_hash, 0);
+		Signature signature = Signature.getInstance("SHA256withRSA");
+		signature.initSign(priv_Wuille);
+		signature.update(tx.getRawDataToSign(0));
+		byte[] sig = signature.sign();
+		tx.addSignature(sig, 0);
+		tx.finalize();
+
+		//Adding genesis output to the UTXPool
+		UTXOPool utxoPool = new UTXOPool();
+		UTXO utxo = new UTXO(tx.getHash(),0);
+		utxoPool.addUTXO(utxo, tx.getOutput(0));
+
+		
+	}		
 
 }
 
