10cf7c455a08e0acb6db5baeaad1ea8b4f21b47d
Daniel <daniel.olayam@gmail.com>
2018-04-13 13:09:16 -0700

    added bitcoin-cli API

--- Scrooge_main.java
+++ Scrooge_main.java
@@ -4,6 +4,7 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import java.math.BigInteger;
 import java.security.*;
 import java.util.Base64;
+import java.io.*;
 
 public class Scrooge_main{
 
@@ -23,6 +24,31 @@ public class Scrooge_main{
 		return hash;
 	}
 
+	//Uses bitcoin client API calls to generate private and public keys for transactions
+	public static String bitcoinAPI(String cmd){
+		try {
+			// Execute command
+			String command = "/opt/bitcoin-0.16.0/bin/bitcoin-cli ";
+			command += cmd;
+			System.out.println("Final command:"+ command);
+			Process process = Runtime.getRuntime().exec(command);
+			process.waitFor();
+			BufferedReader buf = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String line = "";
+			String output = "";
+
+			while ((line = buf.readLine()) != null) {
+				output += line + "\n";
+			}
+			return output;
+		} catch (InterruptedException e) {
+			System.out.println(e);
+		
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		return null;
+	}
 
 	public static void main(String[] args) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
 
@@ -40,14 +66,21 @@ public class Scrooge_main{
 		System.out.printf("Wuillie public key: %s\n",printKey(pub_Wuille.getEncoded()));
 		System.out.println();
 
-		//Generating Hodler keys
+		//Generating Hodler1 keys
 		pair = keyGen.generateKeyPair();
-		PrivateKey priv_Sodler = pair.getPrivate();
-		PublicKey pub_Sodler = pair.getPublic();
-		System.out.printf("Hodler private key: %s\n",printKey(priv_Sodler.getEncoded()));
-		System.out.printf("Hodler public key: %s\n",printKey(pub_Sodler.getEncoded()));
+		PrivateKey priv_Hodler1 = pair.getPrivate();
+		PublicKey pub_Hodler1 = pair.getPublic();
+		System.out.printf("Hodler1 private key: %s\n",printKey(priv_Hodler1.getEncoded()));
+		System.out.printf("Hodler1 public key: %s\n",printKey(pub_Hodler1.getEncoded()));
 
-		//Creating genesis tx
+		//Generating Hodler2 keys
+		pair = keyGen.generateKeyPair();
+		PrivateKey priv_Hodler2 = pair.getPrivate();
+		PublicKey pub_Hodler2 = pair.getPublic();
+		System.out.printf("Hodler2 private key: %s\n",printKey(priv_Hodler2.getEncoded()));
+		System.out.printf("Hodler2 public key: %s\n",printKey(pub_Hodler2.getEncoded()));
+
+		//START - Genesis tx
 		Transaction tx = new Transaction();
 		tx.addOutput(10, pub_Wuille);
 		byte[] initial_hash = BigInteger.valueOf(1695609641).toByteArray();
@@ -58,14 +91,31 @@ public class Scrooge_main{
 		byte[] sig = signature.sign();
 		tx.addSignature(sig, 0);
 		tx.finalize();
+		//END - Genesis tx
 
 		//Adding genesis output to the UTXPool
 		UTXOPool utxoPool = new UTXOPool();
 		UTXO utxo = new UTXO(tx.getHash(),0);
 		utxoPool.addUTXO(utxo, tx.getOutput(0));
 
-		
-	}		
 
+		//START - Testing txs
+		Transaction tx2 = new Transaction();
+		tx.addInput(tx.getHash(),0);
+
+		tx2.addOutput(5, pub_Hodler1);
+		tx2.addOutput(5, pub_Hodler2);
+
+		String result = bitcoinAPI("getnewaddress");
+		System.out.println("Result getnew address: "+ result);
+
+
+		String waka = "dumpprivkey " + result;
+		String result2 = bitcoinAPI(waka);
+		System.out.println("dumpprivkey: "+result2);
+
+
+
+	}
 }
 
