37a95187506b477034287694ff4d183e22c6f942
Daniel <daniel.olayam@gmail.com>
2018-04-14 19:15:18 -0700

    Requimierement 1,5 for tx validation

--- Scrooge_main.java
+++ Scrooge_main.java
@@ -58,29 +58,38 @@ public class Scrooge_main{
 		SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
 		keyGen.initialize(1024, random);	
 
-		//Generating P. Wuille keys
+		// Getting private and public keys from bitcoin API
+		// String result = bitcoinAPI("getnewaddress");
+		// System.out.println("Result getnew address: "+ result);
+		// String waka = "dumpprivkey " + result;
+		// String result2 = bitcoinAPI(waka);
+		// System.out.println("dumpprivkey: "+result2);
+
+		//Generating P. Wuille keys (coinbase transaction)
 		KeyPair pair = keyGen.generateKeyPair();
 		PrivateKey priv_Wuille = pair.getPrivate();
 		PublicKey pub_Wuille = pair.getPublic();
-		System.out.printf("Wuillie private key: %s\n",printKey(priv_Wuille.getEncoded()));
-		System.out.printf("Wuillie public key: %s\n",printKey(pub_Wuille.getEncoded()));
-		System.out.println();
+		// System.out.printf("Wuillie private key: %s\n",printKey(priv_Wuille.getEncoded()));
+		// System.out.printf("Wuillie public key: %s\n",printKey(pub_Wuille.getEncoded()));
 
 		//Generating Hodler1 keys
 		pair = keyGen.generateKeyPair();
 		PrivateKey priv_Hodler1 = pair.getPrivate();
 		PublicKey pub_Hodler1 = pair.getPublic();
-		System.out.printf("Hodler1 private key: %s\n",printKey(priv_Hodler1.getEncoded()));
-		System.out.printf("Hodler1 public key: %s\n",printKey(pub_Hodler1.getEncoded()));
+		// System.out.printf("Hodler1 private key: %s\n",printKey(priv_Hodler1.getEncoded()));
+		// System.out.printf("Hodler1 public key: %s\n",printKey(pub_Hodler1.getEncoded()));
 
 		//Generating Hodler2 keys
 		pair = keyGen.generateKeyPair();
 		PrivateKey priv_Hodler2 = pair.getPrivate();
 		PublicKey pub_Hodler2 = pair.getPublic();
-		System.out.printf("Hodler2 private key: %s\n",printKey(priv_Hodler2.getEncoded()));
-		System.out.printf("Hodler2 public key: %s\n",printKey(pub_Hodler2.getEncoded()));
+		// System.out.printf("Hodler2 private key: %s\n",printKey(priv_Hodler2.getEncoded()));
+		// System.out.printf("Hodler2 public key: %s\n",printKey(pub_Hodler2.getEncoded()));
 
-		//START - Genesis tx
+
+		UTXOPool utxoPool = new UTXOPool();
+
+		//START - tx, Coinbase tx, included in UTXOPool
 		Transaction tx = new Transaction();
 		tx.addOutput(10, pub_Wuille);
 		byte[] initial_hash = BigInteger.valueOf(1695609641).toByteArray();
@@ -91,28 +100,94 @@ public class Scrooge_main{
 		byte[] sig = signature.sign();
 		tx.addSignature(sig, 0);
 		tx.finalize();
-		//END - Genesis tx
-
-		//Adding genesis output to the UTXPool
-		UTXOPool utxoPool = new UTXOPool();
 		UTXO utxo = new UTXO(tx.getHash(),0);
 		utxoPool.addUTXO(utxo, tx.getOutput(0));
+		//END - Coinbase tx
 
 
-		//START - Testing txs
-		Transaction tx2 = new Transaction();
-		tx.addInput(tx.getHash(),0);
+		// //START - tx2 (valid), coinbase input, two valid outputs
+		// Transaction tx2 = new Transaction();
+		// tx2.addInput(tx.getHash(),0);
+		// signature.initSign(priv_Wuille);
+		// signature.update(tx2.getRawDataToSign(0));
+		// sig = signature.sign();
+		// tx2.addSignature(sig, 0);
+		// tx2.addOutput(5, pub_Hodler1);
+		// tx2.addOutput(5, pub_Hodler2);
+		// tx2.finalize();
+		// utxo = new UTXO(tx2.getHash(),0);
+		// utxoPool.addUTXO(utxo, tx2.getOutput(0));
+		// //END - tx2
 
-		tx2.addOutput(5, pub_Hodler1);
-		tx2.addOutput(5, pub_Hodler2);
 
-		String result = bitcoinAPI("getnewaddress");
-		System.out.println("Result getnew address: "+ result);
+		// //START - tx3 (invalid), coinbase input, output higher value than input
+		// Transaction tx3 = new Transaction();
+		// tx3.addInput(tx.getHash(),0);
+		// signature.initSign(priv_Wuille);
+		// signature.update(tx3.getRawDataToSign(0));
+		// sig = signature.sign();
+		// tx3.addSignature(sig, 0);
+		// tx3.addOutput(10.1, pub_Hodler1);
+		// tx3.finalize();
+		// utxo = new UTXO(tx3.getHash(),0);
+		// utxoPool.addUTXO(utxo, tx3.getOutput(0));
+		// utxo = new UTXO(tx3.getHash(),1);
+		// utxoPool.addUTXO(utxo, tx3.getOutput(1));
+		// //END - tx3
 
 
-		String waka = "dumpprivkey " + result;
-		String result2 = bitcoinAPI(waka);
-		System.out.println("dumpprivkey: "+result2);
+		//START - tx4, Coinbase tx, NOT part of UTXOpool
+		Transaction tx4 = new Transaction();
+		tx4.addInput(initial_hash, 0);
+		signature = Signature.getInstance("SHA256withRSA");
+		signature.initSign(priv_Wuille);
+		signature.update(tx4.getRawDataToSign(0));
+		sig = signature.sign();
+		tx4.addSignature(sig, 0);
+		tx4.addOutput(20, pub_Wuille);
+		tx4.finalize();
+		//END - tx4
+
+		// //START - tx5 (invalid), one input (not part of UTXOPool) referencing tx4 
+		// Transaction tx5 = new Transaction();
+		// tx5.addInput(tx4.getHash(),0);
+		// signature.initSign(priv_Wuille);
+		// signature.update(tx5.getRawDataToSign(0));
+		// sig = signature.sign();
+		// tx5.addSignature(sig, 0);
+		// tx5.addOutput(5, pub_Hodler1);
+		// tx5.addOutput(5, pub_Hodler2);
+		// tx5.finalize();
+		// utxo = new UTXO(tx5.getHash(),0);
+		// utxoPool.addUTXO(utxo, tx5.getOutput(0));
+		// //END - tx5
+
+
+		//START - tx6 (invalid), two inputs, one is part of UTXOPool the other one is not
+		Transaction tx6 = new Transaction();
+		tx6.addInput(tx.getHash(),0); //in UTXO
+		tx6.addInput(tx4.getHash(),0);	//not in UTXO
+		signature.initSign(priv_Wuille);
+		signature.update(tx6.getRawDataToSign(0));
+		sig = signature.sign();
+		tx6.addSignature(sig, 0);
+		tx6.addOutput(30, pub_Hodler1);
+		tx6.finalize();
+		utxo = new UTXO(tx6.getHash(),0);
+		utxoPool.addUTXO(utxo, tx6.getOutput(0));
+		//END - tx6
+
+		TxHandler txHandler = new TxHandler(utxoPool);
+		// System.out.println("[FINAL] Tx2.isValidTx: "+ txHandler.isValidTx(tx2));
+		// System.out.println("[FINAL] Tx3.isValidTx: "+ txHandler.isValidTx(tx3));
+		// System.out.println("[FINAL] Tx5.isValidTx: "+ txHandler.isValidTx(tx5));
+		System.out.println("[FINAL] Tx6.isValidTx: "+ txHandler.isValidTx(tx6));
+
+		// System.out.print("handleTxs:"+txHandler.handleTxs(new Transaction[]{tx2}).length);
+
+
+
+
 
 
 
--- TxHandler.java
+++ TxHandler.java
@@ -1,36 +1,105 @@
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.bouncycastle.util.encoders.Hex;
+
+
 public class TxHandler {
 
-    /**
-     * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
-     * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
-     * constructor.
-     */
-    public TxHandler(UTXOPool utxoPool) {
-        // IMPLEMENT THIS
-    }
+	private UTXOPool pool;
 
-    /**
-     * @return true if:
-     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
-     * (2) the signatures on each input of {@code tx} are valid, 
-     * (3) no UTXO is claimed multiple times by {@code tx},
-     * (4) all of {@code tx}s output values are non-negative, and
-     * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
-     *     values; and false otherwise.
-     */
-    public boolean isValidTx(Transaction tx) {
-        // IMPLEMENT THIS
-        return false;
-    }
 
-    /**
-     * Handles each epoch by receiving an unordered array of proposed transactions, checking each
-     * transaction for correctness, returning a mutually valid array of accepted transactions, and
-     * updating the current UTXO pool as appropriate.
-     */
-    public Transaction[] handleTxs(Transaction[] possibleTxs) {
-        // IMPLEMENT THIS
-        return null;
-    }
+	/**
+	 * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
+	 * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
+	 * constructor.
+	 */
+	public TxHandler(UTXOPool utxoPool) {
+		pool = new UTXOPool(utxoPool);
+	}
+
+	public void printPool(){
+		System.out.println("\n=====Printing UTXO pool=====");
+		ArrayList<UTXO> curr_pool = pool.getAllUTXO();
+		for(UTXO i : curr_pool){ 
+			System.out.println("TxHash:" + Hex.toHexString(i.getTxHash()) + " | i:" + i.getIndex());
+		}
+		System.out.println("=====END UTXO pool=====");
+
+	}
+
+
+	/**
+	 * @return true if:
+	 * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
+	 * (2) the signatures on each input of {@code tx} are valid, 
+	 * (3) no UTXO is claimed multiple times by {@code tx},
+	 * (4) all of {@code tx}s output values are non-negative, and
+	 * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+	 *     values; and false otherwise.
+	 */
+	public boolean isValidTx(Transaction tx) {
+		boolean isValid = true;
+		printPool();
+
+		double output_sum = 0;
+		ArrayList<Transaction.Output> outputs = tx.getOutputs();
+		for(Transaction.Output i : outputs){
+			// System.out.printf("outputs:%f\n",i.value);
+			output_sum += i.value;
+		}
+		assert(output_sum > 0);
+		System.out.printf("Final output_sum: %f\n", output_sum);
+
+		double input_sum = 0;
+		ArrayList<Transaction.Input> inputs = tx.getInputs();
+		for(Transaction.Input i : inputs){
+			System.out.println("i.outputIndex:"+i.outputIndex+" | i.prevTxHash:"+Hex.toHexString(i.prevTxHash));
+			UTXO prev_utxo = new UTXO(i.prevTxHash, i.outputIndex);
+			Transaction.Output prev_output = pool.getTxOutput(prev_utxo);
+			//(1) - Checking all outputs claimed are in the current UTXO pool
+			if(prev_output == null){
+				System.out.println("[ERROR] Input is not part of UTXO set, " + Hex.toHexString(i.prevTxHash));
+				isValid = false;
+				break;
+			}
+			input_sum += prev_output.value;
+		}
+
+		//(2) - Verify the signatures on each input are valid
+		for(Transaction.Input i : inputs){
+			UTXO prev_utxo = new UTXO(i.prevTxHash, i.outputIndex);
+			Transaction.Output prev_output = pool.getTxOutput(prev_utxo);
+			//TODO: What message to use for verification?
+			// public static boolean verifySignature(PublicKey pubKey, byte[] message, byte[] signature) {
+			// if(!Crypto.verifySignature(prev_output.address, tx.getRawDataToSign(i.outputIndex), i.signature)){
+			// 	System.out.println("[ERROR] Invalid input signature, " + Hex.toHexString(i.prevTxHash));
+			// 	isValid = false;
+			// 	break;
+			// }
+		}
+
+		// System.out.printf("Final input_sum: %f\n", input_sum);
+		//(5) - Checking sum outputs is less than sum of input
+		if (isValid && input_sum < output_sum){
+			System.out.println("[ERROR] Output sum is greater than input sum");
+			isValid = false;
+		}
+		if(!isValid){
+			return isValid;
+		}
+
+		return isValid;
+	}
+
+	/**
+	 * Handles each epoch by receiving an unordered array of proposed transactions, checking each
+	 * transaction for correctness, returning a mutually valid array of accepted transactions, and
+	 * updating the current UTXO pool as appropriate.
+	 */
+	public Transaction[] handleTxs(Transaction[] possibleTxs) {
+		// IMPLEMENT THIS
+		return null;
+	}
 
 }
